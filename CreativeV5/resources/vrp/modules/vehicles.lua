252bc99f1ef72702d4db0727efbb79068c52dbf4
RenatoNetto <renatonetto35@gmail.com>
2024-12-09 15:36:45 -0300

    Teste
    É nois

---
+++ CreativeV5/resources/vrp/modules/vehicles.lua
@@ -0,0 +1,12 @@
+RegisterServerEvent("CleanVehicle")
+AddEventHandler("CleanVehicle", function(entIndex)
+	local NetworkGetEntityFromNetworkId = NetworkGetEntityFromNetworkId(entIndex)
+	if DoesEntityExist(NetworkGetEntityFromNetworkId) and not IsPedAPlayer(NetworkGetEntityFromNetworkId) and 2 == GetEntityType(NetworkGetEntityFromNetworkId) then
+		SetVehicleDirtLevel(NetworkGetEntityFromNetworkId, 0)
+	end
+end)
+
+local vehicles = {}
+function vRP.vehicleGlobal()
+    return vehicles
+end
